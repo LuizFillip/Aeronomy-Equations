b502a1714d95602d57e4bf9f0f32d8348f0e1d1f
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2023-04-19 12:23:31 -0300

    compute electrons and ions collision frequencies

---
+++ src/freq_collisions.py
@@ -0,0 +1,52 @@
+import numpy as np
+
+
+def electron_neutral_collision(Tn, Nn, Te, ne):
+    """
+    The electron-neutral collision rates (frequency) 
+    Kelley (2009)
+    """
+    return (5.4e-10 * Nn * np.sqrt(Tn) + 
+            (34 + 4.18 * np.log(Te**3 / ne))
+            * ne * Te**(-3/2))
+
+
+class collision_frequencies:
+    """
+    See momentum transfer in Schuck and Nagy 2000
+    """
+    @staticmethod
+    def ion_neutrals(O, O2, N2, T):
+        
+        CO2 = (2.31e-10 * O + 
+              2.59e-11 * O2 * 
+              np.sqrt(T) + 4.13e-10 * N2)
+                
+        CO  = (4.45e-11 * O * np.sqrt(T) + 
+              6.64e-10 * O2 + 6.82e-10 * N2)
+        
+        CN2 = (2.58e-10 * O + 
+              4.49e-10 * O2 + 
+              5.14e-11 * np.sqrt(T))
+                
+        CNO = (2.44e-10 *O +
+              4.27e-10 * O2 + 
+              4.34e-10 * N2)
+        
+        return (CO2 + CO + CN2 + CNO) / 4.0
+    
+    @staticmethod
+    def electrons_neutrals(O, O2, N2, He, H, Te):
+        """Collision frequencies electrons with neutrals """
+        #Must substitute T to Te (electron temperature)
+        CN2 = 2.33e-11 * N2 * Te
+        
+        CO2 = 1.82e-10 * O2 * np.sqrt(Te)
+        
+        CO = 8.90e-11 * O * np.sqrt(Te)
+        
+        CHe = 4.60e-10 * He * np.sqrt(Te)
+        
+        CH = 4.50e-09 * H * np.sqrt(Te)
+        
+        return (CN2 + CO2 + CO + CHe + CH) / 5.0 
