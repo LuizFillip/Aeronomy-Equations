a84b0550b4267cc59fd9000f5da3d96ebd804437
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2023-04-23 18:14:50 -0300

    import settings and get_conductivity

---
+++ src/__init__.py
@@ -0,0 +1,6 @@
+from .conductivities import conductivity, conductivity2
+from .freq_collisions import collision_frequencies, ion_neutral
+from .core import get_conductivity
+import settings as s
+
+s.config_labels()
