e44fd3540a642687de3072f281fdd410da0d944e
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2023-04-23 14:51:52 -0300

    changing for complete form in electron neutrals collision frequency

--- src/freq_collisions.py
+++ src/freq_collisions.py
@@ -10,54 +10,58 @@ def electron_neutral_collision(Tn, Nn, Te, ne):
             (34 + 4.18 * np.log(Te**3 / ne))
             * ne * Te**(-3/2))
 
-def nui_1(Tn, O, O2, N2):
-    """
-    The ion-neutral collision frequency
-    by Bailey and Balan (1996)
-    """
-    term_O = (4.45e-11 * O * np.sqrt(Tn) * 
-              (1.04 - 0.067 * np.log10(Tn))**2)
+
+class ion_neutral:
     
-    term_O2 = 6.64e-10 * O2
-    term_N2 = 6.82e-10 * N2
+    @staticmethod
+    def BB1996(Tn, O, O2, N2):
+        """
+        The ion-neutral collision frequency
+        by Bailey and Balan (1996)
+        """
+        term_O = (4.45e-11 * O * np.sqrt(Tn) * 
+                  (1.04 - 0.067 * np.log10(Tn))**2)
         
-    return term_O + term_O2 + term_N2
+        term_O2 = 6.64e-10 * O2
+        term_N2 = 6.82e-10 * N2
+            
+        return term_O + term_O2 + term_N2
 
-
-def nu_3(O, Tn, Ti):
-    """
-    The ion-neutral collision frequency
-    by Schunk and Nagy, 2000
-
-    Parameters
-    ----------
-    O :  float array
-        Molecular oxygen concentration
-    Tn : float array
-        neutral temperature
-    Ti : float array
-        ion temperature
-    """
-    Tr = (Ti + Tn) / 2
-    return 3.7e-11 * O * np.sqrt(Tr) * (1 - 0.064 * np.log10(Tr))**2
+    @staticmethod
+    def SN2000(O, Tn, Ti):
+        """
+        The ion-neutral collision frequency
+        by Schunk and Nagy, 2000
+    
+        Parameters
+        ----------
+        O :  float array
+            Molecular oxygen concentration
+        Tn : float array
+            neutral temperature
+        Ti : float array
+            ion temperature
+        """
+        Tr = (Ti + Tn) / 2
+        return 3.7e-11 * O * np.sqrt(Tr) * (1 - 0.064 * np.log10(Tr))**2
 
 class collision_frequencies:
     """
-    See momentum transfer in Schuck and Nagy 2000
+    See momentum transfer in Schuck and Nagy 2000.
     """
     @staticmethod
-    def ion_neutrals(O, O2, N2, T):
+    def ion_neutrals(Tn, O, O2, N2):
         
         CO2 = (2.31e-10 * O + 
               2.59e-11 * O2 * 
-              np.sqrt(T) + 4.13e-10 * N2)
+              np.sqrt(Tn) + 4.13e-10 * N2)
                 
-        CO  = (4.45e-11 * O * np.sqrt(T) + 
+        CO  = (4.45e-11 * O * np.sqrt(Tn) + 
               6.64e-10 * O2 + 6.82e-10 * N2)
         
         CN2 = (2.58e-10 * O + 
               4.49e-10 * O2 + 
-              5.14e-11 * np.sqrt(T))
+              5.14e-11 * np.sqrt(Tn))
                 
         CNO = (2.44e-10 *O +
               4.27e-10 * O2 + 
@@ -65,18 +69,20 @@ class collision_frequencies:
         
         return (CO2 + CO + CN2 + CNO) / 4.0
     
+
+    
     @staticmethod
     def electrons_neutrals(O, O2, N2, He, H, Te):
-        """Collision frequencies electrons with neutrals """
-        #Must substitute T to Te (electron temperature)
-        CN2 = 2.33e-11 * N2 * Te
+        """Collision frequencies electrons-neutrals."""
+
+        term_N2 = 2.33e-11 * N2 * (1 - 1.21e-4 * Te)*Te
         
-        CO2 = 1.82e-10 * O2 * np.sqrt(Te)
+        term_O2 = 1.82e-10 * O2 * (1 + 3.6e-2 * np.sqrt(Te))*np.sqrt(Te)
         
-        CO = 8.90e-11 * O * np.sqrt(Te)
+        term_O = 8.90e-11 * O * (1 + 5.7e-4 * Te)*np.sqrt(Te)
         
-        CHe = 4.60e-10 * He * np.sqrt(Te)
+        term_He = 4.60e-10 * He * np.sqrt(Te)
         
-        CH = 4.50e-09 * H * np.sqrt(Te)
+        term_H = 4.50e-09 * H * (1 -1.35e-4 * Te)* np.sqrt(Te)
         
-        return (CN2 + CO2 + CO + CHe + CH) / 5.0 
+        return (term_N2 + term_O2 + term_O + term_He + term_H) / 5.0 
