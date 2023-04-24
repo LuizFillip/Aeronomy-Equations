1a62325260e2934898d4682ee998c53af34f3fae
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2023-04-24 16:14:43 -0300

    update inite

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,6 @@
 from .conductivities import conductivity, conductivity2
 from .freq_collisions import collision_frequencies, ion_neutral
-from .core import get_conductivity
+from .core import cond_from_file, cond_from_models, test_data
 import settings as s
 
 s.config_labels()
