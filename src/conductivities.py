5c794ab3d204925fe4cf6eaeb4b718ddf2a35384
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2023-04-24 16:14:27 -0300

    sep pedersen equation part

--- src/conductivities.py
+++ src/conductivities.py
@@ -5,15 +5,9 @@ class conductivity:
     """Ionospheric conductivity given by Kelley 2009"""
     def __init__(
             self, 
-            Ne, 
-            nue, 
-            nui, 
             B = 0.25e-04,
             mass = "effective"):
         
-        self.Ne = Ne
-        self.nu_e = nue
-        self.nu_i = nui
         self.B = B
         
         if mass == "effective":
@@ -37,80 +31,61 @@ class conductivity:
         """Ion gyro frequency"""
         return (self.charge * self.B / self.ion_mass)
     
-    @property
-    def electron_mobility(self):
+    def electron_mobility(self, nue):
         """Electric mobility for electrons (mass transport) (be)"""
-        return (- self.charge) / (self.electron_mass * self.nu_e)
+        return (- self.charge) / (self.electron_mass * nue)
     
-    @property
-    def ion_mobility(self):
+    def ion_mobility(self, nui):
         """Electric mobility for ions (mass transport) (bi)"""
-        return (self.charge) / (self.ion_mass * self.nu_i)
+        return (self.charge) / (self.ion_mass * nui)
 
-    @property
-    def electron_ratio(self):
-        """
-        Ratio Electron cyclotron and eletron-nutral collision
+    def electron_ratio(self, nue):
+        """Ratio by Electron cyclotron and nue"""
+        return self.electron_cyclotron / nue
+    
+    def ion_ratio(self, nui):
+        """Ratio by ion cyclotron frequency and nui"""
+        return self.ion_cyclotron / nui
+    
+    # testando para o caso de ki / 1 + ki^2
+    def electron_term(self, ne, nue):
+        return ((ne * self.charge / self.B) * 
+                abs(self.electron_ratio(nue)) / (1 + self.electron_ratio(nue)**2))
+    
+    def ion_term(self, ne, nui):
+        return ((ne * self.charge / self.B) *  
+                self.ion_ratio(nui) / (1 + self.ion_ratio(nui)**2))
         
-        """
-        return self.electron_cyclotron / self.nu_e
-    
-    @property
-    def ion_ratio(self):
-        """
-        Ratio by ion cyclotron frequency and ion-neutral collision
-        """
-        return self.ion_cyclotron / self.nu_i
-    
-    
-    @property
-    def parallel(self):
-        """Along magnetic field (B)"""
-        ion_term = 1 / (self.ion_mass * self.nu_i)
-        electron_term = 1 / (self.electron_mass * self.nu_e)
-        
-        return (self.Ne *  cs.elementary_charge**2 * 
-                (electron_term + ion_term))
-    
-   
-    @property
-    def pedersen(self): 
+    def pedersen(self, ne, nui, nue): 
         """Along the electric field and perpendicular to B"""
-        
-        electron_term = (
-            self.electron_mobility / (1 + self.electron_ratio**2)
-            )
-        
-        ion_term = (
-            self.ion_mobility / (1 + self.ion_ratio**2)
-            )
-        
-        return (
-            self.Ne * self.charge * (ion_term - electron_term)
-            )
-    
-    @property
-    def pedersen_F(self):
+        return abs(self.electron_term(ne, nue)) + self.ion_term(ne, nui)
+            
+    def pedersen_F(self, ne, nui):
         """
         For κi >> 1 (above 130 km) the Pedersen conductivity can
         be compute by
         """
-        return self.Ne * self.ion_mass * self.nu_i / self.B**2
+        return ne * self.ion_mass * nui / self.B**2
         
-    @property
-    def hall(self):
+    def parallel(self, ne, nui, nue):
+        """Along magnetic field (B)"""
+        ion_term = 1 / (self.ion_mass * nui)
+        electron_term = 1 / (self.electron_mass * nue)
+        
+        return (ne *  cs.elementary_charge**2 * (electron_term + ion_term))
+    
+    def hall(self, ne, nui, nue):
         """Perpendicular to both electric and B"""
         
         electron_term = (
-            self.electron_ratio**2 / (1 + self.electron_ratio**2)
+            self.electron_ratio(nue)**2 / (1 + self.electron_ratio(nue)**2)
             )
         
         ion_term = (
-            self.ion_ratio**2 / (1 + self.ion_ratio**2)
+            self.ion_ratio(nui)**2 / (1 + self.ion_ratio(nui)**2)
             )
         
-        return (self.Ne * self.charge / 
-                self.B) * (electron_term - ion_term)
+        return (ne * self.charge / self.B) * (electron_term - ion_term)
 
 
 
@@ -121,24 +96,15 @@ class conductivity2:
             self, 
             ne, 
             nue, 
-            nui
-            ):
+            nui,
+            B = 0.25e-4, 
+            mass = "effective"):
         
-        self.nue = nue
         self.ne = ne
         self.nue = nue
         self.nui = nui
-        
-       
-        
-        self.omega_e = self.electrons()
-        self.omega_i = self.ions()
-        
-        
-    #default: Equatorial geomagnetic field in Tesla
-    @staticmethod
-    def ions(B = 0.25e-4, mass = "effective"):
-        """Gyrofrequency for ions in 1/s"""
+        self.B = B
+
         
         if mass == "effective":
             # Effective mass of ions O+, O2+, NO+ in kg
@@ -146,12 +112,12 @@ class conductivity2:
         else:
             ions_mass = cs.proton_mass
             
-        return cs.elementary_charge * B / ions_mass 
-    
-    @staticmethod
-    def electrons(B = 0.25e-4):
+        self.ions_mass = ions_mass
+        
         """Gyrofrequency for electrons in 1/s"""
-        return  cs.elementary_charge * B / cs.electron_mass
+        self.omega_e = cs.elementary_charge * self.B / cs.electron_mass
+        self.omega_i = cs.elementary_charge * self.B / self.ions_mass 
+        
     
     @property
     def R1(self):
@@ -159,20 +125,22 @@ class conductivity2:
                 (self.omega_i / self.nui))
     @property
     def R2(self):
-        return self.R1**2 * self.nue**2 + self.omega_e**2
+        return (1 + self.R1)**2 * self.nue**2 + self.omega_e**2
     
     @property    
     def parallel(self):
         """Parallel conductivity"""
         return ((self.ne * cs.elementary_charge**2) / 
                 (cs.electron_mass * self.nue))
-    @property
-    def hall(self):
-        """Hall conductivity on the field line"""
-        return (self.parallel * self.omega_e * 
-                self.nue**2) / self.R2
+    
     @property
     def pedersen(self):
         """Pedersen conductivity on the field line"""
         return (self.parallel * (1 + self.R1) * 
-                self.nue**2) / self.R2
+                self.nue**2 / self.R2)
+    
+    @property
+    def hall(self):
+        """Hall conductivity on the field line"""
+        return (self.parallel * self.omega_e * 
+                self.nue / self.R2)
