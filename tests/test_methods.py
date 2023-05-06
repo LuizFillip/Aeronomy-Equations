0ab675e670386c13b262c5a3187133607492586c
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2023-05-06 17:10:45 -0300

    updates

--- tests/test_methods.py
+++ tests/test_methods.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu May  4 21:55:54 2023
-
-@author: Luiz
-"""
-
 import numpy as np
 import matplotlib.pyplot as plt
 x = np.arange(10)
@@ -12,15 +5,41 @@ x = np.arange(10)
 y = x**2
 
 
-grad = []
-for i in range(len(x) - 1):
-    num = (y[i + 1] - y[i - 1]) 
-    den = 2 *(x[i + 1] - x[i - 1])
-    grad.append(num / den)
+def gradient_cen_diff(x, y):
+    """
+    Compute the gradient of y with respect to x using central differences.
+    
+    Parameters:
+        x (numpy.ndarray): 1D array of x values.
+        y (numpy.ndarray): 1D array of y values.
+    
+    Returns:
+        numpy.ndarray: 1D array of gradient values.
+    """
+    n = len(x)
+    dx = np.diff(x)
+    dy = np.diff(y)
+    gradient = np.zeros(n)
+    gradient[0] = dy[0] / dx[0]
+    gradient[-1] = dy[-1] / dx[-1]
+    gradient[1:-1] = (dy[1:] / dx[1:] + dy[:-1] / dx[:-1]) / 2
+    #for i in range(1, n - 2):
+    #    gradient[i] = (y[i+1] - y[i-1]) / (x[i+1] - x[i-1])
+    
+    return gradient
     
 
 # np.gradient(x, y)
-
+grad =  gradient_cen_diff(x, y)
 plt.plot(x, y)
-plt.plot(x, np.gradient(y, x, edge_order=2))
-plt.plot(x[1:], grad)
+plt.plot(x, np.gradient(x, y, edge_order = 1))
+plt.plot(x, grad)
+
+def gradient_integrated(Ne, apex):
+    Re = 6370.0
+    L = 1 + (apex / Re)
+    const = (1 / (Ne * Re * pow(L, 3)) )
+    return (const * 
+            np.gradient(Ne * pow(L, 3), 
+                        apex, edge_order = 2))
+
