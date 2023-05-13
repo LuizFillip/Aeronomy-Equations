b7fca54b4bcc14621331f6e9bd85a77dad386810
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2023-05-12 21:57:52 -0300

    compute recombination rate in data frame

--- src/core.py
+++ src/core.py
@@ -1,35 +1,9 @@
-import datetime as dt
-from GEO import sites
 import pandas as pd
 import ionosphere as io
 from models import altrange_models
+from atmosphere import recombination_rate
 
 
-def load_calculate(
-        infile
-        ):
-
-    df =  pd.read_csv(infile, index_col = 0)
-    
-    nu = io.collision_frequencies()
-    
-    df["nui"] = nu.ion_neutrals(
-        df["Tn"], df["O"], 
-        df["O2"], df["N2"]
-        )
-        
-    df["nue"] = nu.electrons_neutrals2(
-        df["O"], df["O2"], df["N2"],
-        df["He"], df["H"], df["Te"]
-    )
-    
-    c = io.conductivity()
-
-    df["perd"] = c.pedersen(
-        df["Ne"], df["nui"], df["nue"]
-        )
-    
-    return df
 
 def compute_parameters(df) -> dict:
     
@@ -85,47 +59,34 @@ def cond_from_file(
 
 
 
-def main():
-
-    glat, glon = sites["saa"]["coords"]
-    
-    kwargs = dict(
-        dn = dt.datetime(2013, 1, 1, 21), 
-        glat = glat, 
-        glon = glon,
-        hmin = 200 
-        )
-    
-    cond_from_models(**kwargs)
-    
-    
-    ds = cond_from_file()
-    
-    ds.loc[ds["apex"] == 350]
 
 
-def test_data(**kwargs):
-    
-    df = altrange_models(**kwargs)
+def load_calculate(infile):
 
+    df = pd.read_csv(infile, index_col = 0)
+    
     nu = io.collision_frequencies()
-
-    nui = nu.ion_neutrals(
+    
+    df["nui"] = nu.ion_neutrals(
         df["Tn"], df["O"], 
         df["O2"], df["N2"]
         )
         
-    nue = nu.electrons_neutrals(
+    df["nue"] = nu.electrons_neutrals2(
         df["O"], df["O2"], df["N2"],
         df["He"], df["H"], df["Te"]
-    )    
-    return pd.DataFrame({"ne": df["ne"].copy(), 
-                         "nui": nui, 
-                         "nue": nue})
+    )
+    
+    c = io.conductivity()
+
+    df["perd"] = c.pedersen(
+        df["Ne"], df["nui"], df["nue"]
+        )
+    
+    df["R"] = recombination_rate(df["O2"], df["N2"])
+    return df
+
+infile = "D:\\FluxTube\\01\\201301010000.txt"
 
 
-
-
-
-
-
+load_calculate(infile)
