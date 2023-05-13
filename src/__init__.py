42d2280bdb0395eecf2f2a01f92c36f6b911a659
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2023-05-12 21:58:21 -0300

    upates

--- src/__init__.py
+++ src/__init__.py
@@ -3,12 +3,13 @@ from .freq_collisions import collision_frequencies, ion_neutral
 from .core import (
     cond_from_file, 
     cond_from_models, 
-    test_data, 
     compute_parameters, 
     load_calculate
     )
 
 from .methods import scale_gradient
+from .run_models import test_data
+
 import settings as s
 
 s.config_labels()
