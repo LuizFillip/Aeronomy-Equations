5a5845a2d48671ca70481db2984965ab5cac7412
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2023-12-08 21:06:37 -0300

    updates

--- src/__init__.py
+++ src/__init__.py
@@ -1,8 +1,6 @@
 from .conductivities import conductivity, conductivity2
 from .freq_collisions import collision_frequencies, ion_neutral
 from .core import conductivity_parameters
-
 from .regions import split_regions
-
 from .methods import scale_gradient
 
